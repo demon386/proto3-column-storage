e34532da38322dc1d6013e0831814d44b3f02878
M. Tong <demon386@gmail.com>
2019-12-29 19:23:38 +0800

    Minor reorg on CMakeLists.txt and record_decoder.h/cpp.

---
+++ record_decoder.cpp
@@ -0,0 +1,41 @@
+#include "record_decoder.h"
+
+namespace proto_column {
+
+RecordDecoder::RecordDecoder(const google::protobuf::Message* message) {
+  message_ = message;
+  reflection_ = message_->GetReflection();
+  reflection_->ListFields(*message_, &fields_);
+}
+
+bool RecordDecoder::GetNextValue(FieldCursor* field_cursor) {
+  const int fields_size = fields_.size();
+  if (field_pos_ >= fields_size) {
+    return false;
+  }
+  const auto* field = fields_[field_pos_];
+  const bool is_repeated = field->is_repeated();
+  const int array_size =
+      is_repeated ? reflection_->FieldSize(*message_, field) : 1;
+  // The initial value |array_pos_| is always valid here for repeated fields.
+  // Because if the field is present, then there's at least one value.
+  DCHECK_LT(array_pos_, array_size);
+  field_cursor->field = field;
+  if (field->is_repeated()) {
+    field_cursor->array_pos = array_pos_;
+  } else {
+    field_cursor->array_pos = -1;
+  }
+
+  // Forward the cursors.
+  if (array_pos_ == array_size - 1) {
+    field_pos_++;
+    array_pos_ = 0;
+  } else {
+    array_pos_++;
+  }
+
+  return true;
+}
+
+}  // namespace proto_column
--- record_decoder.h
+++ record_decoder.h
@@ -10,11 +10,7 @@ namespace proto_column {
 
 class RecordDecoder {
  public:
-  RecordDecoder(const google::protobuf::Message* message) {
-    message_ = message;
-    reflection_ = message_->GetReflection();
-    reflection_->ListFields(*message_, &fields_);
-  }
+  RecordDecoder(const google::protobuf::Message* message);
 
   struct FieldCursor {
     const google::protobuf::FieldDescriptor* field;
@@ -25,35 +21,7 @@ class RecordDecoder {
 
   // Gets next value and move forward. Returns |false| if there's no new value,
   // otherwise returns |true|.
-  bool GetNextValue(FieldCursor* field_cursor) {
-    const int fields_size = fields_.size();
-    if (field_pos_ >= fields_size) {
-      return false;
-    }
-    const auto* field = fields_[field_pos_];
-    const bool is_repeated = field->is_repeated();
-    const int array_size =
-        is_repeated ? reflection_->FieldSize(*message_, field) : 1;
-    // The initial value |array_pos_| is always valid here for repeated fields.
-    // Because if the field is present, then there's at least one value.
-    DCHECK_LT(array_pos_, array_size);
-    field_cursor->field = field;
-    if (field->is_repeated()) {
-      field_cursor->array_pos = array_pos_;
-    } else {
-      field_cursor->array_pos = -1;
-    }
-
-    // Forward the cursors.
-    if (array_pos_ == array_size - 1) {
-      field_pos_++;
-      array_pos_ = 0;
-    } else {
-      array_pos_++;
-    }
-
-    return true;
-  }
+  bool GetNextValue(FieldCursor* field_cursor);
 
   const google::protobuf::Message* message() { return message_; }
 
